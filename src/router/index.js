530d6c151273902a7c07b4a12f7f42b2dbf57b65
Jamshidbekdev <jamshidbek.yoldoshev.99@gmail.com>
2022-08-17 11:47:50 +0500

    route mode has been changed to web

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import user_type from '@/composables/middleware/user_middleware';
 import auth from '@/composables/middleware/auth';
@@ -142,8 +142,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  // history: createWebHistory(process.env.BASE_URL),
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior() {
     document.getElementById('page').scrollIntoView();
